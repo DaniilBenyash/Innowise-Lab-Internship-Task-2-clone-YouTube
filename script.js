e8fcc317ecd71dce2a33ef091d2d7f4a7ede0cb3
Daniil Beniash <daniilbeniash97@gmail.com>
2023-09-14 11:40:06 +0300

    fix: swithTheme code

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 const switcherTheme = document.getElementById("switchTheme")
 
+const THEME = {
+    DARK: 'dark',
+    LIGHT: 'light',
+}
+
 function switchTheme(theme){
     localStorage.setItem('theme', theme)
 
     const logoLight = document.querySelector('#light-logo')
     const logoDark = document.querySelector('#dark-logo')
 
-    if(theme === 'dark') {
-        logoLight.style.display = 'none'
-        logoDark.style.display = 'block'
-    } else {
-        logoDark.style.display = 'none'
-        logoLight.style.display = 'block'
-    }
+    theme === THEME.DARK ? 
+    (logoLight.style.display = 'none', 
+    logoDark.style.display = 'block') 
+    : 
+    (logoDark.style.display = 'none',
+    logoLight.style.display = 'block')
 
-    const elements = document.querySelectorAll('#theme')
+    const body = document.getElementById('theme')
 
-    for(el of elements) el.setAttribute("data-theme", theme)
+    body.setAttribute("data-theme", theme)
 }
 
 function checkTheme(){
     const currentTheme = localStorage.getItem('theme')
 
-    currentTheme === 'light' || currentTheme === null ? switchTheme('light') : switchTheme('dark') 
+    currentTheme === THEME.LIGHT || currentTheme === null ? switchTheme(THEME.LIGHT) : switchTheme(THEME.DARK) 
 }
 checkTheme()
 
 function setTheme(){
     const currentTheme = localStorage.getItem('theme')
 
-    currentTheme === 'light' ? switchTheme('dark') : switchTheme('light')
+    currentTheme === THEME.LIGHT ? switchTheme(THEME.DARK) : switchTheme(THEME.LIGHT)
 }
 
 switcherTheme.addEventListener('click', setTheme)
